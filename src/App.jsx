f44a3a77be0134254cf223f1e708d6586fdd8bbc
REDnjgngim <fbi0d9s4@gmail.com>
2024-11-19 22:11:39 +0900

    スレッドのフィルタ機能追加、ついでにファイル名変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import "./index.css";
-import BbsMessages from "./BbsMessages.jsx";
+import BbsMessages from "./BbsThreads.jsx";
 import FixedFooterButtons from "./FixedFooterButtons.jsx";
 import ModalWindow from "./ModalWindow";
 import Toast from "./Toast";
